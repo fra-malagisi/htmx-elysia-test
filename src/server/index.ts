1c15e4dd1eba4ea26b4a2904d4c6800a93a08481
Francesco <malagisifra@gmail.com>
2023-10-08 14:01:10 +0200

    feat: add pug

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,14 @@
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
-import staticPlugin from "@elysiajs/static";
+const pug = require("pug");
 
-const app = new Elysia().use(cors()).use(staticPlugin());
+const app = new Elysia().use(cors());
 
 app.get("/styles.css", () => Bun.file("dist/styles.css"));
-app.get("/", () => Bun.file("public/pages/index.html"));
+app.get("/", () => {
+  const html = pug.renderFile("public/pages/home.pug");
+  return new Response(html, { headers: { "Content-Type": "text/html" } });
+});
 
 app.get("/random-user", async () => {
   const response = await fetch("https://randomuser.me/api/");
