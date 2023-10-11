2f3b2d0ed35b0022a91944ac7409176bf2b12031
Francesco <malagisifra@gmail.com>
2023-10-11 10:57:21 +0200

    feat: add filter post call

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,4 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { cors } from "@elysiajs/cors";
 const pug = require("pug");
 
@@ -9,32 +9,32 @@ app.get("/", () => {
   const html = pug.renderFile("public/pages/home.pug", {
     nationalities: [
       { value: "", text: "-- Select a nationality" },
-      { value: "AU", text: "Australian" },
-      { value: "BR", text: "Brazilian" },
-      { value: "CA", text: "Canadian" },
-      { value: "CH", text: "Swiss" },
-      { value: "DE", text: "German" },
-      { value: "DK", text: "Danish" },
-      { value: "ES", text: "Spanish" },
-      { value: "FI", text: "Finnish" },
-      { value: "FR", text: "French" },
-      { value: "GB", text: "British" },
-      { value: "IE", text: "Irish" },
-      { value: "IN", text: "Indian" },
-      { value: "IR", text: "Iranian" },
-      { value: "MX", text: "Mexican" },
-      { value: "NL", text: "Dutch" },
-      { value: "NO", text: "Norwegian" },
-      { value: "NZ", text: "New Zealander" },
-      { value: "RS", text: "Serbian" },
-      { value: "TR", text: "Turkish" },
-      { value: "UA", text: "Ukrainian" },
-      { value: "US", text: "American" },
+      { value: "au", text: "Australian" },
+      { value: "br", text: "Brazilian" },
+      { value: "ca", text: "Canadian" },
+      { value: "ch", text: "Swiss" },
+      { value: "de", text: "German" },
+      { value: "dk", text: "Danish" },
+      { value: "es", text: "Spanish" },
+      { value: "fi", text: "Finnish" },
+      { value: "fr", text: "French" },
+      { value: "gb", text: "British" },
+      { value: "ie", text: "Irish" },
+      { value: "in", text: "Indian" },
+      { value: "ir", text: "Iranian" },
+      { value: "mx", text: "Mexican" },
+      { value: "nl", text: "Dutch" },
+      { value: "no", text: "Norwegian" },
+      { value: "nz", text: "New Zealander" },
+      { value: "rs", text: "Serbian" },
+      { value: "tr", text: "Turkish" },
+      { value: "ua", text: "Ukrainian" },
+      { value: "us", text: "American" },
     ],
     genders: [
       { value: "", text: "-- Select a gender" },
-      { value: "M", text: "Male" },
-      { value: "F", text: "Female" },
+      { value: "male", text: "Male" },
+      { value: "female", text: "Female" },
     ],
   });
   return new Response(html, { headers: { "Content-Type": "text/html" } });
@@ -52,6 +52,33 @@ app.get("/random-user", async () => {
   });
   return new Response(html, { headers: { "Content-Type": "text/html" } });
 });
+
+app.post(
+  "/random-user",
+  async ({ body }) => {
+    console.log(body);
+    const url = new URL("https://randomuser.me/api/1.4");
+    const params = new URLSearchParams();
+    params.append("gender", body.gender);
+    params.append("nat", body.nationality);
+    url.search = params.toString();
+    const response = await fetch(url.toString());
+    const data = await response.json();
+    const user = data.results[0];
+    const html = pug.renderFile("public/components/random-user.pug", {
+      name: `${user.name.title}. ${user.name.first} ${user.name.last}`,
+      email: user.email,
+      picture: user.picture.large,
+    });
+    return new Response(html, { headers: { "Content-Type": "text/html" } });
+  },
+  {
+    body: t.Object({
+      gender: t.String(),
+      nationality: t.String(),
+    }),
+  },
+);
 
 app.listen(8080);
 
